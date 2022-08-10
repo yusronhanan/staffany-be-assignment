f0e8a2a7db49568fdb72b8077534746ccc3da7b8
Yusron H <yusronzain@gmail.com>
2022-08-10 11:22:37 +0700

    Add error message constant

---
+++ src/shared/constant/error_message.ts
@@ -0,0 +1,9 @@
+
+export class ErrMessage {
+    static ErrNotFound = "not found";
+    static ErrWeekShiftAlreadyPublished = "week shift on selected date already published";
+    static ErrShiftAlreadyPublished = "shift already published";
+    static ErrShiftClashing = "selected date and time is clashing";
+    static ErrTimeRangeIsInvalid = "selected time range is invalid";
+    static ErrInvalidResponse = "invalid response";
+}
